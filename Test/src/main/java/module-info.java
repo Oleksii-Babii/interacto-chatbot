09e0dbb9ad1932aa7ce8686306ad21e730983da8
oleksii.babii <oleksii.babii@student.griffith.ie>
2024-04-17 18:14:28 +0100

    Test

---
+++ Test/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module griffith.test {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens griffith.test to javafx.fxml;
+    exports griffith.test;
+}
